781e39da5bee0e6598ed49132cd5268c37be4435
Alexander Martin <Alexander@theAlexanderMartin.com>
2020-04-09 20:01:05 -0400

    Added a way to push data to server The pi's will push their data in JSON format to addDataPoint/{PI_ID} and the server will store it in the database.

--- landing/urls.py
+++ landing/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.login, name='index'),
     path('landing/', views.landing, name='index'),
     path('data/<int:pi_id>/', views.data, name='index'),
+    path('addDataPoint/<int:pi_id>', views.addDataPoint, name='index'),  # Post data can't be forwarded
+    path('addDataPoint/<int:pi_id>/', views.addDataPoint, name='index'),
 ]
